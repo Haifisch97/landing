fede060961dbe4b7679f23985c21d94ab467d9da
Haifisch97 <yura24375@gmail.com>
2023-01-19 19:48:39 +0200

    added burger menu

---
+++ css/jscript/scripts.js
@@ -0,0 +1,6 @@
+function activeBurger(){
+    if (document.getElementById("burger-bar").style.display !== "none"){
+    document.getElementById("top-nav").classList.toggle("active");
+    document.getElementById("burger-bar").classList.toggle("change");
+}
+}
